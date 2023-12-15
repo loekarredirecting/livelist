d3d12a8c39bce1e63e70b075dc667338a49e79b3
htetrkar <95409875+loekarredirecting@users.noreply.github.com>
2023-12-15 20:43:28 +0630

    Update mainlive.php

--- mainlive.php
+++ mainlive.php
@@ -10,12 +10,12 @@
 "link": [
 {
 "type": "Vietnam HD",
-"livelink": "0",
+"livelink": "https://live.vbfast.xyz/263164433de10c40396fd969fbe38611/1702692493311/as9xexm/index.m3u8",
 "trylink": "https://xlz.plvb.xyz/"
 },
 {
 "type": "Vietnam FHD",
-"livelink": "0",
+"livelink": "https://acdn.p2pcdn.xyz/xoilac/channel4.m3u8?auth_key=3000001702649314-0-0-21e4c8b424af3050ccb34a5fd367b32e",
 "trylink": ""
 },
 {
@@ -51,12 +51,12 @@
 "link": [
 {
 "type": "Vietnam HD",
-"livelink": "0",
+"livelink": "https://live.vbfast.xyz/8db6a4ff885d88eec8098538d65301e3/1702692544750/kqpqy3v/index.m3u8",
 "trylink": "https://xlz.plvb.xyz/"
 },
 {
 "type": "Vietnam FHD",
-"livelink": "0",
+"livelink": "https://acdn.p2pcdn.xyz/xoilac/channel3.m3u8?auth_key=3000001702649357-0-0-284ad6a9371e08806dddcfdd10d7a54f",
 "trylink": ""
 },
 {
@@ -92,12 +92,12 @@
 "link": [
 {
 "type": "Vietnam HD",
-"livelink": "0",
+"livelink": "https://live.vbfast.xyz/d1f81c58e413081cd4df2c7585d7779f/1702692586174/lhsazur/index.m3u8",
 "trylink": "https://xlz.plvb.xyz/"
 },
 {
 "type": "Vietnam FHD",
-"livelink": "0",
+"livelink": "https://acdn.p2pcdn.xyz/xoilac/channel2.m3u8?auth_key=3000001702649400-0-0-eb1ca60c9eeb5461c51355cdeb6e7f1d",
 "trylink": ""
 },
 {
@@ -133,12 +133,12 @@
 "link": [
 {
 "type": "Vietnam HD",
-"livelink": "0",
+"livelink": "https://live.vbfast.xyz/3380e28f0f9592a383aafb642570b9b8/1702692675670/rchtwtx/rchtwtx@720p.m3u8",
 "trylink": "https://xlz.plvb.xyz/"
 },
 {
 "type": "Vietnam FHD",
-"livelink": "0",
+"livelink": "https://acdn.p2pcdn.xyz/xoilac/channel16.m3u8?auth_key=3000001702649488-0-0-f835a4549146ed31f6e61adeac7a6cc8",
 "trylink": ""
 },
 {
@@ -174,12 +174,12 @@
 "link": [
 {
 "type": "Vietnam HD",
-"livelink": "0",
+"livelink": "https://live.vbfast.xyz/a375204edef3e02a8121d90aa8e4aa9a/1702692720413/en7hf6r/index.m3u8",
 "trylink": "https://xlz.plvb.xyz/"
 },
 {
 "type": "Vietnam FHD",
-"livelink": "0",
+"livelink": "https://acdn.p2pcdn.xyz/xoilac/channel5.m3u8?auth_key=3000001702649532-0-0-332ad0b8d4e57eef502c97685aaed3e1",
 "trylink": ""
 },
 {
@@ -215,12 +215,12 @@
 "link": [
 {
 "type": "Vietnam HD",
-"livelink": "0",
+"livelink": "https://live.vbfast.xyz/be4eea63394870ffbf0ab121e90d5805/1702692764149/xqispup/index.m3u8",
 "trylink": "https://xlz.plvb.xyz/"
 },
 {
 "type": "Vietnam FHD",
-"livelink": "0",
+"livelink": "https://acdn.p2pcdn.xyz/xoilac/channel21.m3u8?auth_key=3000001702649576-0-0-f74701a1e6cf32d7edec23aed3e62754",
 "trylink": ""
 },
 {
