dbbf5d15a0be1880e8e67e71b9b414609f6a4fe6
htetrkar <95409875+loekarredirecting@users.noreply.github.com>
2023-12-15 21:29:17 +0630

    Update slider.php

--- slider.php
+++ slider.php
@@ -1,6 +1,5 @@
 
 [{ "title": "New Live", "link": "https://play.google.com/store/apps/details?id=com.pro.burmese_tv", "img": "https://raw.githubusercontent.com/loekarredirecting/img/main/avietnam.jpg" },
-{ "title": "Facebook", "link": "https://www.facebook.com/profile.php?id=100089461360942", "img": "https://raw.githubusercontent.com/loekarredirecting/img/main/fb_bmtv.gif" }, 
 { "title": "Advertising", "link": "https://play.google.com/store/apps/details?id=com.pro.burmese_tv", "img": "https://raw.githubusercontent.com/loekarredirecting/img/main/adslocal.jpg" }
 
 
